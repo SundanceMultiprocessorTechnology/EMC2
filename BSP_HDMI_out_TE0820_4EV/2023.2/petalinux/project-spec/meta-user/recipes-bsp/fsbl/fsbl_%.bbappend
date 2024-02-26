4273a874d354f10c8550b63c2f1996147595973a
EmilieWheatley <emilie.w@sundance.com>
2024-02-26 10:11:44 +0000

    add HDMI out project

--- BSP_HDMI_out_TE0820_4EV/2023.2/petalinux/project-spec/meta-user/recipes-bsp/fsbl/fsbl_%.bbappend
+++
@@ -1,2 +0,0 @@
-#Enable appropriate FSBL debug flags  
-YAML_COMPILER_FLAGS_append = " -DFSBL_DEBUG_INFO"
---
+++ BSP_HDMI_out_TE0820_4EV/2023.2/prebuilt/releases.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+rm ./boot/*
+rm ./boot/standalone/*
+rm ./firmware/*
+rm ./petalinux/*
+rm ./software/baremetal/*
+rm ./software/baremetal_classic/*
+rm ./software/linux/*
+cp ../boot/BOOT.bin ./boot
+cp ../boot/standalone/BOOT.bin ./boot/standalone
+cp ../firmware/*.runs/impl_1/*.bit ./firmware
+cp ../firmware/*.xsa ./firmware
+cp ../petalinux/images/linux/*.elf ./petalinux
+cp ../petalinux/images/linux/boot.scr ./petalinux
+cp ../petalinux/images/linux/*.bit ./petalinux
+cp ../petalinux/images/linux/image.ub ./petalinux
+find ../software/baremetal -type f | grep -i ".*.elf$"  | xargs -I '{}' cp '{}' ./software/baremetal
+find ../software/baremetal_classic -type f | grep -i ".*.elf$"  | xargs -I '{}' cp '{}' ./software/baremetal_classic
+find ../software/linux -type f -executable  | xargs -I '{}' cp '{}' ./software/linux/
+
+
